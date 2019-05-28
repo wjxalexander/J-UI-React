48de4afd1b5940bef3ce5a4191353431d42e8f8a
wjxalexander <wjxalexander@hotmail.com>
2019-05-28 21:14:28 +0800

    webpack1

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,6 @@
 console.log('hi')
+
+console.log("hi1")
+const div = document.createElement('div')
+div.innerText = "wwwwww"
+document.body.appendChild(div)
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -53,5 +54,15 @@ module.exports = {
   externals: {
     react: "React",
     "react-dom": "ReactDOM"
-  }
+  },
+  /*The HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles.
+   This is especially useful for webpack bundles that include a hash in the filename which 
+   changes every compilation. You can either let the plugin generate an HTML file for you, 
+   supply your own template using lodash templates, or use your own loader.
+   https://github.com/jantimon/html-webpack-plugin#options
+   简单来说就是帮你在index.html中自动加入<script src="index.js"></script>
+   */
+  plugins: [
+    new HtmlWebpackPlugin({ title: "J-UI-React", template: "index.html" })
+  ]
 };
