78bf135ea3b5b303abd45a6f0c180d96a28a667e
wjxalexander <wjxalexander@hotmail.com>
2019-06-02 22:00:25 +0800

    icon first step

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,6 @@
-import Button from "./button";
 import React from 'react';
 import ReactDOM from "react-dom";
-ReactDOM.render(<Button></Button>,document.querySelector("#root"))
+import  Icon from "./icon";
+
+ReactDOM.render(<Icon iconName="weChat" />,document.querySelector("#root"));
 
