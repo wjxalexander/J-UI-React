f9df946a0ef5f4726a7764869f17c3a08804a482
wjxalexander <wjxalexander@hotmail.com>
2019-07-05 18:45:19 +0800

    alert

--- examples/dialog.example.tsx
+++ examples/dialog.example.tsx
@@ -1,17 +1,20 @@
 import * as React from "react"
-import Dialog from "../lib/Dialog/dialog"
+import Dialog, {alert} from "../lib/Dialog/dialog"
 import Button from "../lib/Button/button"
 
 const {useState} = React;
 
 export default function () {
   const [x, setX] = useState(false);
-  const disMiss = ()=>setX(false);
+  const disMiss = () => setX(false);
   return (<div>
     <Button buttonType="default" onClick={() => {setX(!x)}}/>
     <Dialog onDismiss={disMiss} visible={x} buttons={[<Button buttonType="default" title="保存"/>,
       <Button onClick={disMiss} buttonType="custom" title="取消"/>]}>
       <div>1231213</div>
     </Dialog>
+    <div>---------</div>
+    <Button buttonType="default" onClick={() => {alert(<div>1234</div>)}}/>
+
   </div>)
 }
--- lib/Dialog/dialog.tsx
+++ lib/Dialog/dialog.tsx
@@ -2,6 +2,7 @@ import React, {Fragment, ReactElement} from "react"
 import "./dialog.style.scss"
 import {Icon, Button} from "../index";
 import {rootClass} from "../utils/classFactory";
+import ReactDOM from "react-dom";
 
 interface dialogProps {
   visible: boolean;
@@ -22,40 +23,52 @@ const buttons = (buttonArray: Array<ReactElement>): ReactElement => {
 // HIGH ORDER FUNCTION
 const classGenerator = rootClass('j-ui-dialog');
 
-
 const Dialog: React.FunctionComponent<dialogProps> = (props: dialogProps) => {
   const onDismiss: React.MouseEventHandler<HTMLOrSVGElement> = (event: React.MouseEvent<HTMLOrSVGElement>) => {
     props.onDismiss(event)
   };
   const MaskDismiss: React.MouseEventHandler<HTMLOrSVGElement> = (event: React.MouseEvent<HTMLOrSVGElement>) => {
-    if (!props.maskClosable) {
-      return
-    }
+    if (!props.maskClosable) return;
     props.onDismiss(event)
   };
+  const portalContent = (props.visible ?
+    <Fragment>
+      <div className={classGenerator("mask")} onClick={MaskDismiss}/>
+      <div className={classGenerator("container")}>
+        <Icon iconName={'Cancel'} USEMsFabricIcon
+              className={classGenerator("close")}
+              onClick={onDismiss}/>
+        <header className={classGenerator("header")}>All emails together</header>
+        <main className={classGenerator("content")}>{props.children}</main>
+        <footer className={classGenerator("footer")}>
+          {props.buttons && props.buttons.length > 0 ? (buttons(props.buttons)) :
+            (<Fragment>
+              <span><Button buttonType="default" title="Save"/></span>
+              <span><Button buttonType="custom" title="Cancel" onClick={onDismiss}/></span>
+            </Fragment>)}
+        </footer>
+      </div>
+    </Fragment> : null);
   return (
-    props.visible ?
-      <Fragment>
-        <div className={classGenerator("mask")} onClick={MaskDismiss}/>
-        <div className={classGenerator("container")}>
-          <Icon iconName={'Cancel'} USEMsFabricIcon
-                className={classGenerator("close")}
-                onClick={onDismiss}/>
-          <header className={classGenerator("header")}>All emails together</header>
-          <main className={classGenerator("content")}>{props.children}</main>
-          <footer className={classGenerator("footer")}>
-            {props.buttons && props.buttons.length > 0 ? (buttons(props.buttons)) :
-              (<Fragment>
-                <span><Button buttonType="default" title="Save"/></span>
-                <span><Button buttonType="custom" title="Cancel"/></span>
-              </Fragment>)}
-          </footer>
-        </div>
-      </Fragment> : null
-
+    // React 传送门
+    ReactDOM.createPortal(portalContent, document.body)
   )
 };
 Dialog.defaultProps = {
   maskClosable: true
 };
+const alert = (content: React.ReactNode) => {
+
+  const div = document.createElement("div");
+  document.body.append(div);
+  const component = <Dialog onDismiss={() => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove()
+  }} visible={true}>{content}</Dialog>;
+
+  ReactDOM.render(component, div);
+};
+
+export {alert}
 export default Dialog
