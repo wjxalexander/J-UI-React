d31f2ddef3ba8dae12fa72dbb5133fe9d813d5ce
wjxalexander <wjxalexander@hotmail.com>
2019-07-10 07:48:01 +0800

    dialog com

--- examples/dialog.example.tsx
+++ examples/dialog.example.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Dialog, {alert} from "../lib/Dialog/dialog"
+import Dialog, {alert, confirm,modal} from "../lib/Dialog/dialog"
 import Button from "../lib/Button/button"
 
 const {useState} = React;
@@ -7,6 +7,7 @@ const {useState} = React;
 export default function () {
   const [x, setX] = useState(false);
   const disMiss = () => setX(false);
+  const openModal = ()=> { const close = modal(<h1>hello<Button buttonType="default" title='close' onClick={()=>close()}/></h1>,true)};
   return (<div>
     <Button buttonType="default" onClick={() => {setX(!x)}}/>
     <Dialog onDismiss={disMiss} visible={x} buttons={[<Button buttonType="default" title="保存"/>,
@@ -15,6 +16,10 @@ export default function () {
     </Dialog>
     <div>---------</div>
     <Button buttonType="default" onClick={() => {alert(<div>1234</div>)}}/>
+    <div>---------</div>
+    <Button buttonType="default" onClick={() => {confirm('1', () => {console.log('yes')}, () => {console.log('no')})}}/>
+    <div>---------</div>
+    <Button buttonType="custom" onClick={openModal}/>
 
   </div>)
 }
--- lib/Dialog/dialog.tsx
+++ lib/Dialog/dialog.tsx
@@ -1,82 +1,105 @@
-import React, {Fragment, ReactElement} from "react"
+import React, {Fragment, ReactElement, ReactNode} from "react"
 import "./dialog.style.scss"
 import {Icon, Button} from "../index";
 import {rootClass} from "../utils/classFactory";
 import ReactDOM from "react-dom";
 
 interface dialogProps {
-    visible: boolean;
-    children?: React.ReactNode
-    buttons?: Array<ReactElement>;
-    onDismiss: React.MouseEventHandler<HTMLOrSVGElement | React.ReactNode>;
-    maskClosable?: boolean;
-    showCloseButton?: boolean;
+  visible: boolean;
+  children?: React.ReactNode
+  buttons?: Array<ReactElement>;
+  onDismiss: React.MouseEventHandler<HTMLOrSVGElement | React.ReactNode>;
+  maskClosable?: boolean;
+  showCloseButton?: boolean;
+  noButton?: boolean;
+  title?: React.ReactNode;
 }
 
 // button Generation
 const buttons = (buttonArray: Array<ReactElement>): ReactElement => {
-    return (
-        <Fragment>
-            {buttonArray.map((item, index) => (<span key={`${index}-${Math.random()}`}>{item}</span>))}
-        </Fragment>
-    )
+  return (
+    <Fragment>
+      {buttonArray.map((item, index) => (<span key={`${index}-${Math.random()}`}>{item}</span>))}
+    </Fragment>
+  )
 };
 // HIGH ORDER FUNCTION
 const classGenerator = rootClass('j-ui-dialog');
 
 const Dialog: React.FunctionComponent<dialogProps> = (props: dialogProps) => {
-    const onDismiss: React.MouseEventHandler<HTMLOrSVGElement> = (event: React.MouseEvent<HTMLOrSVGElement>) => {
-        props.onDismiss(event)
-    };
-    const MaskDismiss: React.MouseEventHandler<HTMLOrSVGElement> = (event: React.MouseEvent<HTMLOrSVGElement>) => {
-        if (!props.maskClosable) return;
-        props.onDismiss(event)
-    };
-    const portalContent = (props.visible ?
-        <Fragment>
-            <div className={classGenerator("mask")} onClick={MaskDismiss}/>
-            <div className={classGenerator("container")}>
-                {props.showCloseButton ? (<Icon iconName={'Cancel'} USEMsFabricIcon
-                                                className={classGenerator("close")}
-                                                onClick={onDismiss}/>) : null}
-                <header className={classGenerator("header")}>All emails together</header>
-                <main className={classGenerator("content")}>{props.children}</main>
-                <footer className={classGenerator("footer")}>
-                    {props.buttons && props.buttons.length > 0 ? (buttons(props.buttons)) :
-                        (<Fragment>
-                            <span><Button buttonType="default" title="Save"/></span>
-                            <span><Button buttonType="custom" title="Cancel" onClick={onDismiss}/></span>
-                        </Fragment>)}
-                </footer>
-            </div>
-        </Fragment> : null);
-    return (
-        // React 传送门
-        ReactDOM.createPortal(portalContent, document.body)
-    )
+  const onDismiss: React.MouseEventHandler<HTMLOrSVGElement> = (event: React.MouseEvent<HTMLOrSVGElement>) => {
+    props.onDismiss(event)
+  };
+  const MaskDismiss: React.MouseEventHandler<HTMLOrSVGElement> = (event: React.MouseEvent<HTMLOrSVGElement>) => {
+    if (!props.maskClosable) return;
+    props.onDismiss(event)
+  };
+  const portalContent = (props.visible ?
+    <Fragment>
+      <div className={classGenerator("mask")} onClick={MaskDismiss}/>
+      <div className={classGenerator("container")}>
+        {props.showCloseButton ? (<Icon iconName={'Cancel'} USEMsFabricIcon
+                                        className={classGenerator("close")}
+                                        onClick={onDismiss}/>) : null}
+        <header className={classGenerator("header")}>{props.title || "提示"}</header>
+        <main className={classGenerator("content")}>{props.children}</main>
+        <footer className={classGenerator("footer")}>
+          {props.buttons && props.buttons.length > 0 ? (buttons(props.buttons)) :
+            props.noButton ? null : (<Fragment>
+              <span><Button buttonType="default" title="Save"/></span>
+              <span><Button buttonType="custom" title="Cancel" onClick={onDismiss}/></span>
+            </Fragment>)}
+        </footer>
+      </div>
+    </Fragment> : null);
+  return (
+    // React 传送门
+    ReactDOM.createPortal(portalContent, document.body)
+  )
 };
 Dialog.defaultProps = {
-    maskClosable: true,
-    showCloseButton: true,
+  maskClosable: true,
+  showCloseButton: true,
+  noButton: false
 };
 const alert = (content: React.ReactNode) => {
-    const div = document.createElement("div");
-    document.body.append(div);
-    const disMiss = (component: React.ReactElement, div: HTMLElement) => {
-        ReactDOM.render(React.cloneElement(component, {visible: false}), div);
-        ReactDOM.unmountComponentAtNode(div);
-        div.remove()
-    };
-    const component = <Dialog showCloseButton={false}
-                              onDismiss={() => disMiss(component, div)}
-                              visible={true}
-                              buttons={[<Button
-                                  buttonType="default"
-                                  title="确定"
-                                  onClick={() => disMiss(component, div)}/>]}>{content}</Dialog>;
-
-    ReactDOM.render(component, div);
+  const button = [<Button buttonType="default" title="确定" onClick={() => onClose()}/>];
+  const onClose = modal(content, false, button)
+};
+const confirm = (content: React.ReactNode, confirm?: () => void, cancel?: () => void) => {
+  const div = document.createElement("div");
+  document.body.append(div);
+  const onYes = () => {
+    onClose()
+    confirm && confirm()
+  };
+  const onNo = () => {
+    onClose();
+    cancel && cancel()
+  };
+  const buttons = [
+    <Button buttonType="default" title="Confirm" onClick={onYes}/>,
+    <Button buttonType="default" title="Cancel" onClick={onNo}/>
+  ];
+  const onClose = modal(content, false, buttons)
 };
 
-export {alert}
+
+const modal = (content: ReactNode, noButton?: boolean, buttons?: Array<ReactElement>,) => {
+  const disMiss = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove()
+  };
+  const component = <Dialog showCloseButton={false}
+                            onDismiss={disMiss}
+                            visible={true}
+                            noButton={noButton}
+                            buttons={buttons}>{content}</Dialog>;
+  const div = document.createElement("div");
+  document.body.append(div);
+  ReactDOM.render(component, div);
+  return disMiss;
+};
+export {alert, confirm, modal}
 export default Dialog
