cad56cdc09eb29992e46f68df568abfe08c5cade
wjxalexander <wjxalexander@hotmail.com>
2019-09-03 22:26:28 +0800

    dialog test

--- lib/Dialog/__tests__/dialog.unit.jsx
+++ lib/Dialog/__tests__/dialog.unit.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Dialog from '../dialog';
 import Enzyme, {mount, shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import renderer from "react-test-renderer";
 
 Enzyme.configure({ adapter: new Adapter() });
 
